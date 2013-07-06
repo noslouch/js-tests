172aa52db30fdb2a8edc588988925a252bb99bbf
brian <me@brianwhitton.com>
2013-07-06 15:54:33 -0400

    my answers

--- app/arrays.js
+++ app/arrays.js
@@ -3,59 +3,103 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+        return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+        var total = 0;
+        for (var i = 0; i < arr.length; i++){
+            if (typeof arr[i] === 'number'){
+                total += arr[i];
+            }
+        }
+        return total;
     },
 
     remove : function(arr, item) {
-
+        var i = arr.indexOf(item);
+        while (i !== -1){
+            arr.splice(i, 1);
+            i = arr[i] === item ? i : arr.indexOf(item, i+1);
+        }
+        return arr;
     },
 
     removeWithoutCopy : function(arr, item) {
-
+        var i = arr.indexOf(item);
+        while (i !== -1){
+            arr.splice(i, 1);
+            i = arr[i] === item ? i : arr.indexOf(item, i+1);
+        }
+        return arr;
     },
 
     append : function(arr, item) {
-
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend : function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail : function(arr) {
-
+        arr.shift();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
-
+        return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-
+        arr.splice(index, 0, item);
+        return arr;
     },
 
     count : function(arr, item) {
-
+        var count = 0;
+        var i = arr.indexOf(item);
+        while (i !== -1){
+            count++;
+            i = arr.indexOf(item, i+1);
+        }
+        return count;
     },
 
     duplicates : function(arr) {
-
+        var dupes = [];
+        for (var i = 0; i < arr.length; i++){
+            var a = arr[i];
+            if (arr.indexOf(a, i+1) !== -1 && dupes.indexOf(a) === -1){
+                dupes.push(a);
+            }
+        }
+        return dupes;
     },
 
     square : function(arr) {
-
+        var result = [];
+        for (var i = 0; i < arr.length; i++){
+            result.push(Math.pow(arr[i],2));
+        }
+        return result;
     },
 
     findAllOccurrences : function(arr, target) {
-
+        var pos = [];
+        for (var i = 0; i < arr.length; i++){
+            if (arr[i] === target){
+                pos.push(i);
+            }
+        }
+        return pos;
     }
   };
 });
--- app/functions.js
+++ app/functions.js
@@ -3,19 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+        return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+        return function(args){
+            return str + ', ' + args;
+        };
     },
 
     makeClosures : function(arr, fn) {
-
+        var result = [];
+        function makeFunc(x){
+            return function(){ return fn(x); };
+        }
+        for (var i = 0; i < arr.length; i++){
+            result.push(makeFunc(arr[i]));
+        }
+        return result;
     },
 
     partial : function(fn, str1, str2) {
